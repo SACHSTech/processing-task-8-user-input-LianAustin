54da770d55cc4d95dc7929ac8917e26f494a340e
LianAustin <austin.lian25@ycdsbk12.ca>
2024-05-10 11:11:07 -0400

    test push

--- Sketch.java
+++ Sketch.java
@@ -14,6 +14,7 @@ public class Sketch extends PApplet {
   /** 
    * Called once at the beginning of execution.  Add initial set up
    * values here i.e background, stroke, fill etc.
+   *
    */
   public void setup() {
     background(210, 255, 173);
