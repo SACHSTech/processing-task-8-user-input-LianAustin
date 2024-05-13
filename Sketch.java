7b77283ea252c69595866b4eb79fe7d517d568ee
LianAustin <austin.lian25@ycdsbk12.ca>
2024-05-13 14:11:11 -0400

    Added javadoc comments

--- Sketch.java
+++ Sketch.java
@@ -34,7 +34,10 @@ public class Sketch extends PApplet {
       text("a key is pressed!!!", width / 5, height / 5);
     }
   }
-  
+  /**
+   * Description: This method is called when the mouse is clicked. It will load a random flower image and display it at the mouse's location.
+   * @author: Austin L
+   */
   public void mouseClicked() {
     String[] flowerFiles = {"BlueFlower.png", "OrangeFlower.png", "PinkFlower.png"};
     int intIndex = (int) random(flowerFiles.length);
@@ -43,12 +46,21 @@ public class Sketch extends PApplet {
     flowerImage.resize((int)(flowerImage.width * fltScale), (int)(flowerImage.height * fltScale)); // Resize the image
     image(flowerImage, mouseX - flowerImage.width / 2, mouseY - flowerImage.height / 2); // Center the image on the cursor
   }
+  /**
+   * Description: This method is called when the mouse is dragged. It will load a grass image and display it at the mouse's location. The grass image will be centered on the cursor.
+   * @author: Austin L
+   */
   public void mouseDragged() {
     PImage grassImage = loadImage("BetterGrassImage.png");
     float fltScale = 0.05f; // Further reduce scale factor to make the grass even smaller
     grassImage.resize((int)(grassImage.width * fltScale), (int)(grassImage.height * fltScale)); // Resize the image
     image(grassImage, mouseX - grassImage.width / 2, mouseY - grassImage.height / 2); // Center the image on the cursor
   }
+  /**
+   * Description: This method is called when the mouse wheel is scrolled. It will load a butterfly image and display it at the mouse's location. The butterfly image will be centered on the cursor.
+   * @param event: The mouse event that is triggered when the mouse wheel is scrolled.
+   * @author: Austin L
+   */
   public void mouseWheel(MouseEvent event) {
     String[] butterflyFiles = {"Butterfly.png"};
     int intIndex = (int) random(butterflyFiles.length);
@@ -60,6 +72,14 @@ public class Sketch extends PApplet {
 
   boolean boolBPressed = false;
   boolean boolRPressed = false;
+  /**
+   * Description: This method is called when a key is pressed. It will display text based on the key pressed.
+   * If the 'B' key is pressed, the background will turn blue. If the 'R' key is pressed, the background will turn red.
+   * If both the 'B' and 'R' keys are pressed, the background will turn purple.
+   * The boolean variables boolBPressed and boolRPressed are used to keep track of whether the 'B' and 'R' keys are pressed, respectively.
+   * If the 'B' or 'R' key is released, the corresponding boolean variable will be set to false.
+   * @author: Austin L
+   */
   public void keyPressed() {
     if (keyCode == UP) {
       text("up arrow", width / 3, height / 2);
@@ -84,6 +104,11 @@ public class Sketch extends PApplet {
       background(128, 0, 128);
     }
   }
+  /**
+   * Description: This method is called when a key is released.
+   * It will set the boolean variables boolBPressed and boolRPressed to false if the 'B' or 'R' key is released, respectively.
+   * @author: Austin L
+   */
   public void keyReleased() {
     if (key == 'B' || key == 'b') {
       boolBPressed = false;
